a732a3a40e4a61a82bd0f73e0bd8c5ca333fe3e8
George Munyoro <george@munyoro.com>
2023-06-02 23:26:24 +0200

    feat: base averages view

--- apps/mobile/pages/AveragesRoute.tsx
+++ apps/mobile/pages/AveragesRoute.tsx
@@ -1,9 +1,46 @@
 import { View, Text, StyleSheet } from "react-native";
+import { DataTable } from "react-native-paper";
+import { useStore } from "ui/store";
+import { Time } from "ui/store/types";
+import { formatTimeMS } from "ui/utils/formatting";
 
 const AveragesRoute = () => {
+  const { selectedSession, sessions } = useStore((state) => state);
+
   return (
     <View style={styles.container}>
       <Text>Averages</Text>
+      <DataTable>
+        <DataTable.Header>
+          <DataTable.Title textStyle={{ color: "white" }} numeric>
+            Time
+          </DataTable.Title>
+          <DataTable.Title textStyle={{ color: "white" }} numeric>
+            AO5
+          </DataTable.Title>
+          <DataTable.Title textStyle={{ color: "white" }} numeric>
+            AO12
+          </DataTable.Title>
+        </DataTable.Header>
+
+        {sessions
+          .find((s) => s.name === selectedSession)
+          ?.times.map(({ id, time, ao5, ao12 }: Time) => {
+            return (
+              <DataTable.Row borderless={true} key={id}>
+                <DataTable.Cell textStyle={{ color: "white" }} numeric>
+                  {formatTimeMS(time)}
+                </DataTable.Cell>
+                <DataTable.Cell textStyle={{ color: "white" }} numeric>
+                  {ao5 === 0 ? "-" : formatTimeMS(ao5)}
+                </DataTable.Cell>
+                <DataTable.Cell textStyle={{ color: "white" }} numeric>
+                  {ao12 === 0 ? "-" : formatTimeMS(ao12)}
+                </DataTable.Cell>
+              </DataTable.Row>
+            );
+          })}
+      </DataTable>
     </View>
   );
 };
